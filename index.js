2ea7ad950d84ba505cc1f00cb2ae1a6ac0b196dd
jarryingnut <rabhishekranjan@gmail.com>
2022-09-21 12:07:28 +0530

    test reverse string

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ export const capitalize = (word) => {
   let capital = word[0].toUpperCase() + word.slice(1, word.length);
   return capital;
 };
+
+export const reverseWord = (word) => {
+  return word.split("").reverse().join("");
+};
